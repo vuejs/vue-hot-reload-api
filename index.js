3f48776b8b7f6911a320db634a6215123efd3f57
Evan You <yyx990803@gmail.com>
2017-02-25 22:16:10 -0500

    compat with Vue esm builds

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.install = function (vue, browserify) {
   if (installed) return
   installed = true
 
-  Vue = vue
+  Vue = vue.__esModule ? vue.default : vue
   version = Vue.version.split('.').map(Number)
   isBrowserify = browserify
 
