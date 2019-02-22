0215e75840829c98eba9ad75d3015c0dea7eceb0
Thorsten Lünborg <t.luenborg@googlemail.com>
2019-02-22 15:04:44 +0100

    fix: argument order for _u() (close #73) (#74)

--- src/index.js
+++ src/index.js
@@ -256,7 +256,15 @@ function patchScopedSlots (instance) {
   if (!instance._u) return
   // https://github.com/vuejs/vue/blob/dev/src/core/instance/render-helpers/resolve-scoped-slots.js
   const original = instance._u
-  instance._u = slots => original(slots, true)
+  instance._u = slots => {
+    try {
+      // 2.6.4 ~ 2.6.6
+      return original(slots, true)
+    } catch (e) {
+      // 2.5 / >= 2.6.7
+      return original(slots, null, true)
+    }
+  }
   return () => {
     instance._u = original
   }
